8e70fe6696263072cfd7d7e983a9ef31ddfa3b50
hennadiy-aliiev <you@example.com>
2017-04-04 13:37:33 +0300

    update

--- email.py
+++ email.py
@@ -1,8 +1,13 @@
+"""
+This module sorts all emails from your "input_file" and
+additionally chooses emails starting with desired character.
+"""
+
 import re
 import sys
 
 
-def sorting(input_file, outputfile1, outputfile2):
+def sorting(input_file, outputfile1, outputfile2, char):
     with open(input_file) as f:
         emails = f.readlines()
         emails.sort()
@@ -12,7 +17,7 @@ def sorting(input_file, outputfile1, outputfile2):
                 f1.write(line)
 
     with open(outputfile2, "w") as f2:
-            pattern = r"y"
+            pattern = char
             for line in emails:
                 if re.match(pattern, line):
                     f2.write(line)
@@ -20,7 +25,15 @@ def sorting(input_file, outputfile1, outputfile2):
 
 if __name__ == "__main__":
 
-    input_file = sys.argv[1]
-    outputfile1 = sys.argv[2]         
-    outputfile2 = sys.argv[3]
-    sorting(input_file, outputfile1, outputfile2)
+    try:
+        input_file = sys.argv[1]
+        outputfile1 = sys.argv[2]         
+        outputfile2 = sys.argv[3]
+        char = sys.argv[4]
+        sorting(input_file, outputfile1, outputfile2, char)
+    except:
+        print("""ERROR! Please enter correct command!
+        Example: "python email.py origin.txt output1.txt output2.txt k" where:
+        origin.txt - file with all your emails,
+        output1.txt - new output file with sorted emails,
+        output2.txt k - new output file with emails starting with k character.""")
